ee803f80e1f525a0936b8a44f3088711588f3ecd
Kate Kryvopustova <k.kryvopustova@gmail.com>
2021-04-27 12:40:32 +0300

    change linebreak from CRLF into LF

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoApp from './components/TodoApp/TodoApp'
+import TodoApp from './components/TodoApp/TodoApp';
 import todoItems from './mocks/initialTodoList';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -8,5 +8,5 @@ ReactDOM.render(
   <React.StrictMode>
     <TodoApp initItems={todoItems} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
